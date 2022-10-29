687a0fedf550f4ac76bb7f2f38c027c858355bfa
ganijo123 <pythondev112@gmail.com>
2022-10-29 10:43:39 +0500

    www

--- data/config.py
+++ data/config.py
@@ -5,8 +5,8 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+BOT_TOKEN = '5406652340:AAFwewyBxfdkrTs6U_6jth0oQL5ogEj3jWQ'  # Bot toekn
+ADMINS = '1892438581'  # adminlar ro'yxati
+IP = "ip"  # Xosting ip manzili
 
 kanallar = ['@sinov003','@sinov0033']
--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from utils.db_api.baza_bilan_ishlash import Database
 from data import config
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+bot = Bot(token='5406652340:AAFwewyBxfdkrTs6U_6jth0oQL5ogEj3jWQ', parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
